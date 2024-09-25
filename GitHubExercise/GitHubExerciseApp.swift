6fdc11e415fb9a89156f193b56e1ad96e14c147f
Jared Cordray <upshotsdg@gmail.com>
2024-09-25 19:21:11 -0400

    Initial Commit

---
+++ GitHubExercise/GitHubExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubExerciseApp.swift
+//  GitHubExercise
+//
+//  Created by Jared Cordray on 9/25/24.
+//
+
+import SwiftUI
+
+@main
+struct GitHubExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
