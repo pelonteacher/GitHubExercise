bad2353f2b3d334095add9b9b2196ae3d571f0c1
Jared Cordray <upshotsdg@gmail.com>
2024-09-26 17:23:34 -0400

    testing commit

--- GitHubExercise/ContentView.swift
+++ GitHubExercise/ContentView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
+            Image(systemName: "1.circle")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("V. 1.0")
         }
         .padding()
     }
